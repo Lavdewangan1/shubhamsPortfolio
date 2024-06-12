403d3182257b374d938cfeac9f38709b8c6a5aeb
Lavdewangan1 <lavkd001@gmail.com>
2024-06-12 15:25:29 +0530

    fourth commit

--- utils/api.js
+++ utils/api.js
@@ -2,10 +2,19 @@ import fs from "fs";
 import { join } from "path";
 import matter from "gray-matter";
 
-const postsDirectory = join(process.cwd(), "_posts");
+const postsDirectory = join(__dirname, "_posts"); // Use __dirname to get the current directory
 
 export function getPostSlugs() {
-  return fs.readdirSync(postsDirectory);
+  try {
+    return fs.readdirSync(postsDirectory);
+  } catch (error) {
+    if (error.code === 'ENOENT') {
+      console.log('Warning: _posts directory does not exist');
+      return [];
+    } else {
+      throw error;
+    }
+  }
 }
 
 export function getPostBySlug(slug, fields = []) {
@@ -25,7 +34,7 @@ export function getPostBySlug(slug, fields = []) {
       items[field] = content;
     }
 
-    if (typeof data[field] !== "undefined") {
+    if (typeof data[field]!== "undefined") {
       items[field] = data[field];
     }
   });
@@ -36,8 +45,8 @@ export function getPostBySlug(slug, fields = []) {
 export function getAllPosts(fields = []) {
   const slugs = getPostSlugs();
   const posts = slugs
-    .map((slug) => getPostBySlug(slug, fields))
+   .map((slug) => getPostBySlug(slug, fields))
     // sort posts by date in descending order
-    .sort((post1, post2) => (post1.date > post2.date ? -1 : 1));
+   .sort((post1, post2) => (post1.date > post2.date? -1 : 1));
   return posts;
 }
